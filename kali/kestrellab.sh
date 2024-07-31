309bc8d6731babd1d0dc6a1b4ae1221da98d3d2a
Xiaokui Shu <subbyte@gmail.com>
2024-07-30 23:57:27 -0400

    update kestrel lab script

--- kali/kestrellab.sh
+++ kali/kestrellab.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/sh
 
-echo "killing lab if exists..."
-docker ps | grep opencybersecurityalliance | cut -d ' ' -f1 | xargs docker stop
+echo "#### Kestrel Lab for Black Hat US 2024 ####"
+echo ""
 
-echo "spinning up fresh lab..."
+echo "Killing lab if exists..."
+docker ps | grep opencybersecurityalliance | cut -d ' ' -f1 | xargs docker stop
+echo ""
+
+echo "Spinning up fresh lab..."
 nohup repo2docker https://github.com/opencybersecurityalliance/black-hat-us-2024.git &
+echo ""
 
 while true
 do
@@ -12,11 +17,11 @@ do
     container=$(docker ps | grep opencybersecurityalliance)
     if [ -z "$container" ]
     then
-        echo "building lab..."
+        echo "#### Building lab..."
     else
         port=$(docker ps | grep opencybersecurityalliance | cut -d ':' -f2 | cut -d '-' -f1)
-        echo "ready, hunt"
-        sleep 1
+        echo "#### Ready, set, hunt"
+        sleep 2
         firefox http://127.0.0.1:$port/tree
         break
     fi

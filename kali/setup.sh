63c95b09d7a87a01c620ff0b5e62d0f46cb177ba
Xiaokui Shu <subbyte@gmail.com>
2024-07-30 23:28:01 -0400

    fix kali install

--- kali/setup.sh
+++ kali/setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/sh
 
+# install docker
 sudo apt update
 sudo apt install -y docker.io
 sudo systemctl enable docker --now
 sudo usermod -aG docker $USER
 
-pip install jupyter-repo2docker
+# install repo2docker
+sudo pip install jupyter-repo2docker
 
+# setup launch script
 sudo curl -o /usr/share/icons/kestrel.svg https://raw.githubusercontent.com/opencybersecurityalliance/kestrel-lang/develop/logo/logo.svg
 sudo curl -o /usr/share/applications/launcher.desktop https://raw.githubusercontent.com/opencybersecurityalliance/black-hat-us-2024/main/kali/launcher.desktop
 sudo curl -o /usr/bin/kestrellab.sh https://raw.githubusercontent.com/opencybersecurityalliance/black-hat-us-2024/main/kali/kestrellab.sh

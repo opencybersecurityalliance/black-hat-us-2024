220d10b30a652c133d321cb2bb5ae5900d61930e
Xiaokui Shu <subbyte@gmail.com>
2024-07-30 23:42:57 -0400

    improve kali install

--- kali/README.md
+++ kali/README.md
@@ -1,3 +1,17 @@
+# Black Hat US 2024 Arsenal Lab Setup
+
+## 1. Install
 ```
 curl https://raw.githubusercontent.com/opencybersecurityalliance/black-hat-us-2024/main/kali/setup.sh | sh -
 ```
+
+## 2. Reboot
+
+## 3. Build Container
+```
+repo2docker https://github.com/opencybersecurityalliance/black-hat-us-2024.git
+```
+
+## 4. Launch Lab with Gnome Launcher
+
+Type `kestrel` in gnome launcher to pop the lab to launch
--- kali/kestrellab.sh
+++ kali/kestrellab.sh
@@ -10,10 +10,12 @@ do
     container=$(docker ps | grep opencybersecurityalliance)
     if [ -z "$container" ]
     then
-        echo "wait for container to go up..."
+        echo "build container..."
     else
         port=$(docker ps | grep opencybersecurityalliance | cut -d ':' -f2 | cut -d '-' -f1)
-        firefox http://localhost:$port/tree
+        echo "let's hunt..."
+        sleep 2
+        firefox http://127.0.0.1:$port/tree
         break
     fi
 done
